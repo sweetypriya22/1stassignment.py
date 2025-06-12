75606d31b0eddff8c96a4b368e8cbc3518857948
sweetypriya22 <sweetyp901@gmail.com>
2025-06-12 20:27:38 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+print("hello world")
+a = input('addition:')
+b = input('sub:')
+c = input('multiply:')
+d = input('division:')
+a=int(a)
+b=int(b)
+c=int(c)
+d=int(d)
+Sum = a + b 
+Minus = a-b
+mul = a*c
+if d != 0:
+	div = a/d
+else:
+	div = "Error: Division by zero"
+print(Sum)
+print(Minus)
+print(mul)
+print(div)
+
+
+a = input('Entre your first name:')
+b = input('Entre your last name:')
+print("Hello",a+""+b,"Welcome to python programming")
